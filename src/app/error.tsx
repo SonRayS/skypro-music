29ae395feb94523989015f9ba2f46f626aece046
Illia Hryn <grinpis01@gmail.com>
2024-06-20 15:48:41 +0200

    save err style

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -17,20 +17,25 @@ export default function Error({
 
     return (
         <>
-            <div className={styles.PageNotFound}>
-                <Image
-                    className={styles.PageNotFoundImg}
-                    src="/img/Xm404.gif"
-                    alt="Page not found :C"
-                    width={500}
-                    height={500}
-                />
-            </div>
-            <div className={styles.PageNotFoundImg}>
-                <h2>Что-то пошло не так!</h2>
-                <button className={styles.ModalButton} onClick={reset}>
-                    <p className={styles.ModalBtnText}>Попробовать снова</p>
-                </button>
+            <div className={styles.errContainer}>
+                <div className={styles.err}>
+                    <Image
+                        className={styles.errImg}
+                        src="/img/smile_sad.png"
+                        alt="Error-ka :C"
+                        width={120}
+                        height={120}
+                    />
+                </div>
+                <div className={styles.PageNotFoundImg}>
+                    <h1>Ошибка загрузки</h1>
+                    <p className={styles.errBtnText}>
+                        Проверьте подключение к сети и повторите попытку
+                    </p>
+                    <button className={styles.errButton} onClick={reset}>
+                        <p className={styles.errBtnText}>Повторить</p>
+                    </button>
+                </div>
             </div>
         </>
     );
