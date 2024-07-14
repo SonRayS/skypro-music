6b2fb9220277665cc77a6a097f269a7b011f714c
Illia Hryn <grinpis01@gmail.com>
2024-07-14 22:45:27 +0200

    save change ( actual error )

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,30 @@
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import styles from "./not-found.module.css";
 
 export default function NotFoundPage() {
+    const router = useRouter();
+
+    const click = () => {
+        router.push("/");
+    };
     return (
         <>
-            <div>
-                <h1>Page not fond !</h1>
-                <Link href="/">Go to main page</Link>
+            <div className={styles.Selector404}>
+                <Image
+                    className={styles.Selector404Img}
+                    src="/img/404.png"
+                    alt="@#$%^ :C"
+                    width={1024}
+                    height={512}
+                />
+                <h1 className={styles.Selector404Title}>
+                    Page not fond @#$%^! :__C
+                </h1>
+                <button onClick={click} className={styles.Selector404Btn}>
+                    <p className={styles.Selector404BtnText}>Go to main page</p>
+                </button>
             </div>
         </>
     );
