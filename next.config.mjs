eb7d3ab548476565587c362a06dc92909dee98cd
Illia Hryn <grinpis01@gmail.com>
2024-07-14 16:24:18 +0200

    err wtf

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
             {
                 source: "/",
-                destination: "/playlist",
+                destination: "/page",
                 permanent: true,
             },
         ];
--- src/app/components/page/mainPage.tsx
+++ src/app/components/page/mainPage.tsx
@@ -2,14 +2,17 @@ import Selector from "../selector/selector";
 import Header from "../headerMenu/headerMenu";
 import Body from "../body/bodyMainComponent/bodyMainComponent";
 import RightBar from "../rightBar/rightBar";
+import RootLayout from "@/app/layout";
 
 function MainPage() {
     return (
-        <Selector>
-            <Header />
-            <Body />
-            <RightBar />
-        </Selector>
+        <RootLayout>
+            <Selector>
+                <Header />
+                <Body />
+                <RightBar />
+            </Selector>
+        </RootLayout>
     );
 }
 
