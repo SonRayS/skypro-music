721f179d1200ccbadfe0b940bc1966bd7a98ce9a
Illia Hryn <grinpis01@gmail.com>
2024-07-27 20:12:07 +0200

    useCallback

--- src/app/myTracks/page.tsx
+++ src/app/myTracks/page.tsx
@@ -31,7 +31,7 @@ export default function MainTracks() {
     }, [dispatch, token, router]);
 
     useEffect(() => {
-        getMyTracks;
+        getMyTracks();
     }, [getMyTracks]);
 
     return (
