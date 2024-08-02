300cbb401c14d6f0d7af04f2eea0057785cf4cd6
Illia Hryn <grinpis01@gmail.com>
2024-08-02 03:02:30 +0200

    memo

--- src/app/components/body/bodyMainComponent/bodyGetTrack/bodyGetTrack.tsx
+++ src/app/components/body/bodyMainComponent/bodyGetTrack/bodyGetTrack.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import Search from "../../bodySearch/bodySearch";
 import Filters from "../../bodyFilters/Filters";
 import TrackHeader from "../../bodyTrackHeder/bodyTrackHeder";
@@ -11,7 +12,6 @@ import {
     setFilterPlaylist,
     setIsFavorite,
 } from "@/store/features/playlistSlice";
-import { useEffect } from "react";
 
 type getTrackType = {
     tracksData: trackType[];
@@ -20,64 +20,72 @@ type getTrackType = {
     onTrackUpdate?: () => void;
 };
 
-export default function BodyGetTrack({
-    tracksData,
-    params,
-    isFavorite,
-    onTrackUpdate,
-}: getTrackType) {
-    const dispatch = useAppDispatch();
-    const filterList = useAppSelector((state) => state.playlist.filterList);
-    const filtersName = useAppSelector((state) => state.playlist.filtersName);
+const BodyGetTrack = React.memo(
+    ({ tracksData, params, isFavorite, onTrackUpdate }: getTrackType) => {
+        const dispatch = useAppDispatch();
+        const filterList = useAppSelector((state) => state.playlist.filterList);
+        const filtersName = useAppSelector(
+            (state) => state.playlist.filtersName
+        );
 
-    useEffect(() => {
-        dispatch(setIsFavorite({ isFavorite }));
-        dispatch(setFilterPlaylist({ filterPlaylist: tracksData }));
-        dispatch(resetSearchFilters({ filterPlaylist: tracksData }));
-    }, [dispatch, tracksData, isFavorite]);
+        useEffect(() => {
+            dispatch(setIsFavorite({ isFavorite }));
+            dispatch(setFilterPlaylist({ filterPlaylist: tracksData }));
+            dispatch(resetSearchFilters({ filterPlaylist: tracksData }));
+        }, [dispatch, tracksData, isFavorite]);
 
-    const filteredTracks = filtersName.length > 0 ? filterList : tracksData;
+        const filteredTracks = filtersName.length > 0 ? filterList : tracksData;
 
-    let mainTitle = "";
+        let mainTitle = "";
 
-    if (params) {
-        if (params === "1") {
-            mainTitle = "Плейлист дня";
-        } else if (params === "2") {
-            mainTitle = "100 танцевальных хитов";
-        } else if (params === "3") {
-            mainTitle = "Инди-заряд";
+        if (params) {
+            if (params === "1") {
+                mainTitle = "Плейлист дня";
+            } else if (params === "2") {
+                mainTitle = "100 танцевальных хитов";
+            } else if (params === "3") {
+                mainTitle = "Инди-заряд";
+            } else {
+                mainTitle = "Мой плейлист";
+            }
         } else {
-            mainTitle = "Мой плейлист";
+            mainTitle = "Треки";
         }
-    } else {
-        mainTitle = "Треки";
-    }
 
-    return (
-        <div className={classNames(styles.mainCenterBlock, styles.centerBlock)}>
-            <Search />
-            <h2 className={styles.centerBlockH2}>{mainTitle}</h2>
-            <Filters tracks={tracksData} />
+        return (
             <div
                 className={classNames(
-                    styles.centerBlockContent,
-                    styles.playlistContent
+                    styles.mainCenterBlock,
+                    styles.centerBlock
                 )}
             >
-                <TrackHeader />
-                {filteredTracks.length === 0
-                    ? "Нет треков :C"
-                    : filteredTracks.map((el) => (
-                          <TrackComponent
-                              key={el.id}
-                              track={el}
-                              tracksData={tracksData}
-                              isFavorite={isFavorite}
-                              onTrackUpdate={onTrackUpdate}
-                          />
-                      ))}
+                <Search />
+                <h2 className={styles.centerBlockH2}>{mainTitle}</h2>
+                <Filters tracks={tracksData} />
+                <div
+                    className={classNames(
+                        styles.centerBlockContent,
+                        styles.playlistContent
+                    )}
+                >
+                    <TrackHeader />
+                    {filteredTracks.length === 0
+                        ? "Нет треков :C"
+                        : filteredTracks.map((el) => (
+                              <TrackComponent
+                                  key={el.id}
+                                  track={el}
+                                  tracksData={tracksData}
+                                  isFavorite={isFavorite}
+                                  onTrackUpdate={onTrackUpdate}
+                              />
+                          ))}
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
+);
+
+BodyGetTrack.displayName = "BodyGetTrack";
+
+export default BodyGetTrack;
